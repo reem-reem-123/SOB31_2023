8c59bf6491c70fcf9e7bf6ebbbae45c0d8578c59
reem-reem-123 <rr1069@live.mdx.ac.uk>
2023-11-27 23:47:43 +0400

    Revert "Merge branch 'main' of https://github.com/reem-reem-123/SOB31_2023"
    This reverts commit e1eb7e7a4d23d1029018f7bc76f6db8516cc557a, reversing
    changes made to 6f690fa29b4a1ae3f2d9ac7f18a647d22d34ffc7.

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -1,25 +1,21 @@
 import random
 
-cowbullcount=[]                                         #declared list
 def compare_numbers(number, user_guess):
-    current=0
-    cows=0                                              #initializing variables
-    bulls=0  
-    for i in user_guess:
-        if i in number:
-            cows+=1
-            if number[current]==i:
-                bulls+=1
-        current+=1                                      
-
-    cowbullcount=[cows, bulls]                           
-    return cowbullcount                                  #changed 'cowbull' to 'cowbullcount'
+    ## your code here
+    return cowbull
 
 playing = True #gotta play the game
+<<<<<<< HEAD
 number = str(random.randint(1000,9999)) #random 4 digit number 
 guesses = 0
 # removed the 'print(number)' statement                                  
 print(number)
+=======
+number = str(random.randint(0,9999)) #random 4 digit number
+guesses = 0
+print number
+
+>>>>>>> parent of b0b67cd (Fixed Files)
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
 print("For every number that exists in the sequence but is in wrong place, you get a cow. For every one in the right place, you get a bull.")
@@ -27,7 +23,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = input("Give me your best guess!")       #replaced 'raw_input' with input()
+    user_guess = raw_input("Give me your best guess!")
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
@@ -37,7 +33,7 @@ while playing:
 
     if cowbullcount[1]==4:
         playing = False
-        print("You win the game after " + str(guesses) + " guesses! The number was "+str(number)) #added guesses
+        print("You win the game after " + str(guesses) + "! The number was "+str(number))
         break #redundant exit
     else:
         print("Your guess isn't quite right, try again.")
